9d7a1cad39591d9f8e9449f1f5df01b679cb0a17
liuqi <liuqi6602@163.com>
2024-03-14 23:03:44 +0800

    feat: asf

--- app.vue
+++ app.vue
@@ -2,6 +2,10 @@
   <nuxt-page />
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+useHead({
+  link: [{ rel: 'shortcut icon', href: './fissure-logo-white.svg' }]
+})
+</script>
 
 <style lang="scss" scoped></style>
---
+++ components/wt-header.vue
@@ -0,0 +1,21 @@
+<template>
+  <section class="wt-header">
+    <el-row justify="space-between" items="center" class="h-100%">
+      <el-col :span="4">
+        <el-text size="large">warframe team</el-text>
+      </el-col>
+      <el-col :span="4">
+        <wt-theme />
+      </el-col>
+    </el-row>
+  </section>
+</template>
+
+<script setup lang="ts"></script>
+
+<style lang="scss" scoped>
+.wt-header {
+  padding: 0 2rem;
+  height: 57px;
+}
+</style>
