7bf2b5e1851edadad5e37b54d364e582df5f0eaf
liuqi <liuqi6602@163.com>
2024-03-17 19:35:29 +0800

    ci: test jenkins

--- components/wt-features.vue
+++ components/wt-features.vue
@@ -1,6 +1,6 @@
 <template>
   <el-row :gutter="14">
-    <el-col :xs="24" :sm="12" :md="8" :xl="6" class="item" v-for="item in 10">
+    <el-col :xs="24" :sm="12" :md="8" :xl="6" class="item" v-for="item in 2">
       <nuxt-link to="/fissure" style="text-decoration: none">
         <el-card>
           <p>裂缝</p>
