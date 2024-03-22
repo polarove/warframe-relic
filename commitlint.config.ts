4d5859b260fe2ab7db4e8ba1bb37d2bef14e0894
liuqi <liuqi6602@163.com>
2024-03-23 06:14:42 +0800

    其他: 清理依赖

--- commitlint.config.ts
+++
@@ -1,7 +0,0 @@
-let pathToTempfileOfCommitMessage = undefined
-process.argv.forEach((val, index, array) => {
-  console.log(index + ': ' + val)
-  if (val === '--edit') {
-    const pathToTempOfCommitMessage = val[index + 1]
-  }
-})
