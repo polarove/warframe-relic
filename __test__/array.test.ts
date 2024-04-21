1b3a8e52bc84ff3d26a9d4b2e30c3b5328f81c1f
liuqi <liuqi6602@163.com>
2024-04-21 21:38:48 +0800

    测试：更新测试用例

--- __test__/array.test.ts
+++ __test__/array.test.ts
@@ -2,30 +2,45 @@ import { VoidUtil } from '@polaris_liu/toolcat'
 import { expect, test } from 'vitest'
 import { reactive } from 'vue'
 
-test('删除数组', () => {
-  const testFissure = [
-    {
-      id: '1'
-    },
-    {
-      id: '2'
-    },
-    {
-      id: '3'
-    },
-    {
-      id: '4'
-    }
-  ]
+interface numberList {
+  [key: string]: number
+}
+
+const buildArray = (
+  valueStarts: number,
+  valueEnds: number,
+  key: string = 'id'
+) => {
+  let arr: numberList[] = []
+  const build = (value: number): numberList[] => {
+    if (value <= valueEnds) {
+      const el = Object.defineProperty({}, key, {
+        value: valueStarts,
+        enumerable: true,
+        writable: true
+      })
+      arr.push(el)
+      return build((valueStarts += 1))
+    } else return arr
+  }
+  return build(valueStarts)
+}
+
+test('创建数组', () => {
+  expect(buildArray(1, 10).length === 10).toBe(true)
+})
+
+test('过滤数组', () => {
+  const testFissure = buildArray(1, 4)
   const expected = [
     {
-      id: '1'
+      id: 1
     },
     {
-      id: '2'
+      id: 2
     },
     {
-      id: '4'
+      id: 4
     }
   ]
   testFissure.splice(2, 1)
@@ -48,4 +63,19 @@ test('过滤数组', () => {
 
 test('删除数组', () => {
   expect([1, 2, 3, 4].splice(4).length === 0).toBe(true)
+})
+
+test('过滤出不同元素', () => {
+  const testFissure = buildArray(1, 20)
+  const current = buildArray(1, 12)
+  const expected = buildArray(13, 20)
+  expected.push({ id: 3 })
+  current.splice(2, 1)
+  const newFissures = testFissure.filter((fissure) =>
+    VoidUtil.isVoid(current.find((exist) => fissure.id === exist.id))
+  )
+  const matches = newFissures.every((item) =>
+    expected.find((e) => e.id === item.id)
+  )
+  expect(matches).toBe(true)
 })
